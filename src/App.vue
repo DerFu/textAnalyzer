9880445b802d1ea003e6187e99b156031875ab0a
Der Fu <fu@funami.de>
2018-05-25 21:39:51 +0200

    chore(frontend): Add prototype

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
     <div class="flex">
-      <div><textarea name="" id="" cols="30" rows="10" v-model="text"></textarea></div>
-      <div>{{formatedTextObject}}</div>
-      <button @click="transformTextToObject(text)">ok</button>
+      <div>
+        <div v-for="(p, i) in formatedTextObject" :key="i" class="paragraph">
+          <div v-for="(s,i) in p" :key="i" class="sentence">
+            <span v-for="(w, i) in s" :key="i" :class="w.type || 'default'">{{w.value || w}}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <textarea v-model="text" name="text" id="" cols="30" rows="10" @input="transformTextToObject(text)"></textarea>    
+      </div>
     </div>
   </div>
 </template>
@@ -52,8 +59,24 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  line-height: 2.44rem;
+}
+div.paragraph {
+  margin: 0 0 0.5em 0;
+}
+div.sentence {
+  display: inline-block;
+}
+span {
+  background: #ececec;
+  margin: 0 0.1em;
+  padding: 0.1rem 0.3rem;
+  border-radius: 3px;
+}
+span.article {
+  background: green;
+  color: white;
 }
 </style>
